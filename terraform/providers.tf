eb808557c615df0a13710cfb5a04e6b8a38bbb4f
nirmiz <miz10nir@gmail.com>
2024-07-06 23:44:55 +0300

    fixes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,8 +8,8 @@ terraform {
   backend "s3" {
     bucket  = "tf-state-mizrahi"
     key     = "terraform.tfstate"
-    region  = var.region
-    profile = var.AWS_PROFILE
+    region  = "eu-central-1"
+    profile = "nir.mizrahi"
   }
 }
 
@@ -27,12 +27,4 @@ provider "aws" {
   region  = var.region
   profile = var.AWS_PROFILE
 }
-
-provider "kubectl" {
-  host                   = aws_eks_cluster.eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks-cluster.token
-  load_config_file       = false
-}
-
 
