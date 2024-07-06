424170d2c304840aabab6073e0c1f75fb56bc0ad
Nir Mizrahi <87423500+nirmiz@users.noreply.github.com>
2024-07-06 22:17:53 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,86 @@
-# EKS CLUSTER ROLE
-resource "aws_iam_role" "EKSClusterRole" {
-  name = "EKSClusterRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      },
-    ]
-  })
+module "aws_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
-//This policy provides Kubernetes the permissions it requires to manage resources on your behalf. Kubernetes requires Ec2:CreateTags permissions to place identifying information on EC2 resources including but not limited to Instances, Security Groups, and Elastic Network Interfaces
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.EKSClusterRole.name
+# EKS Cluster
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = var.cluster_config.name
+  role_arn = aws_iam_role.EKSClusterRole.arn
+  version  = var.cluster_config.version
+
+  vpc_config {
+    subnet_ids         = flatten([module.aws_vpc.public_subnets, module.aws_vpc.private_subnets])
+    security_group_ids = [module.aws_vpc.default_security_group_id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    module.aws_vpc
+  ]
+
 }
 
-# NODE GROUP ROLE
-resource "aws_iam_role" "NodeGroupRole" {
-  name = "EKSNodeGroupRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+# NODE GROUP
+resource "aws_eks_node_group" "node-ec2" {
+  for_each        = { for node_group in var.node_groups : node_group.name => node_group }
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = each.value.name
+  node_role_arn   = aws_iam_role.NodeGroupRole.arn
+  subnet_ids      = flatten(module.aws_vpc.private_subnets)
+
+  scaling_config {
+    desired_size = try(each.value.scaling_config.desired_size, 2)
+    max_size     = try(each.value.scaling_config.max_size, 3)
+    min_size     = try(each.value.scaling_config.min_size, 2)
+  }
+
+  update_config {
+    max_unavailable = try(each.value.update_config.max_unavailable, 1)
+  }
+
+  ami_type       = each.value.ami_type
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
+  disk_size      = each.value.disk_size
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy
+  ]
 }
 
-//This policy allows Amazon EKS worker nodes to connect to Amazon EKS Clusters.
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.NodeGroupRole.name
+resource "aws_ecr_repository" "testapp" {
+  name = "testapp"
 }
 
-// Provides read-only access to Amazon EC2 Container Registry repositories.
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.NodeGroupRole.name
-}
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  create_namespace = true
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
 
-// This policy provides the Amazon VPC CNI Plugin (amazon-vpc-cni-k8s) the permissions it requires to modify the IP address configuration on your EKS worker nodes. This permission set allows the CNI to list, describe, and modify Elastic Network Interfaces on your behalf
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.NodeGroupRole.name
+  values = [
+    <<EOF
+server:
+  service:
+    type: LoadBalancer
+EOF
+  ]
 }
