b48224921be217311085f40d261a84e3945ed412
Nir Mizrahi <87423500+nirmiz@users.noreply.github.com>
2024-07-06 22:18:22 +0300

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,7 +5,3 @@ data "aws_region" "current" {}
 data "aws_eks_cluster" "eks-cluster" {
   name = aws_eks_cluster.eks-cluster.name
 }
-
-locals {
-  oidc = trimprefix(data.aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer, "https://")
-}
